3ddcbe4cc4d38ce873403e0ec3d126890c8df7b5
Nilavra Bhattacharya | নীলাভ্র ভট্টাচার্য্য <12046196+nilavra@users.noreply.github.com>
2023-01-23 12:08:33 -0600

    compile from windows

--- scripts_and_filters/install_packages_if_missing.R
+++ scripts_and_filters/install_packages_if_missing.R
@@ -1,4 +1,4 @@
-required_packages <- c("rmarkdown", "bookdown", "knitr", "kableExtra", "tidyverse", "here")
+required_packages <- c("rmarkdown", "bookdown", "knitr", "kableExtra", "tidyverse", "here", "downlit", "tufte", "tufterhandout")
 
 for (package in required_packages) {
   print(paste0("checking for install of ", package))
