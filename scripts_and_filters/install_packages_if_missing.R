91f7356c7f373255202b13b831081930a76e8511
Nilavra Bhattacharya | নীলাভ্র ভট্টাচার্য্য <12046196+nilavra@users.noreply.github.com>
2023-01-23 20:20:38 -0600

    graphics working from within powerpoint

--- scripts_and_filters/install_packages_if_missing.R
+++ scripts_and_filters/install_packages_if_missing.R
@@ -1,4 +1,4 @@
-required_packages <- c("rmarkdown", "bookdown", "knitr", "kableExtra", "tidyverse", "here", "downlit", "bslib", "tufte", "tufterhandout")
+required_packages <- c("rmarkdown", "bookdown", "knitr", "kableExtra", "tidyverse", "here", "downlit", "bslib", "tufte", "tufterhandout", "rsvg")
 
 for (package in required_packages) {
   print(paste0("checking for install of ", package))
