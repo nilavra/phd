994b31f237400391bf0118281e8ef388c241eba5
Nilavra Bhattacharya | নীলাভ্র ভট্টাচার্য্য <12046196+nilavra@users.noreply.github.com>
2023-01-30 23:53:09 -0600

    bengali working?

--- test.r
+++
@@ -1,6 +0,0 @@
-print(paste("How", "are", "you?", "you?"))
-if (!require("remotes")) {
-    install.packages("remotes", repos = "https://cran.rstudio.org")
-}
-remotes::install_github("rstudio/bookdown")
-remotes::install_github("ismayc/thesisdown")
